89413a7fbfe1c008364207b34b8f3733bd978e06
adhadiajiputra20 <adhadiajiputra@gmail.com>
2022-09-18 10:19:51 +0800

    penambahan sesuai tantangan promise

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null
+const promiseOutput = async (param) => {
+  let data1 = await promiseTheaterIXX()
+  let data2 = await promiseTheaterVGC()
+  let alldata = await data1.concat(data2)
+  let filtered = alldata.filter(item => item.hasil == param)
+  return filtered.length
+}
 
 module.exports = {
   promiseOutput,
